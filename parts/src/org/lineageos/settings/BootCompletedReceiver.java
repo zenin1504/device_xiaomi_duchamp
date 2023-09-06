71fc79f99f0e5ba04df97d8c7da25c8cb3518084
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-06 10:52:43 +0530

    duchamp: Introduce displayfeature color service and use MIUI color modes
    This service relays the current AOSP color mode to the Xiaomi
    displayfeature HAL, matching stock behaviour. This is necessary to
    avoid colors being reset after turning the screen off, because
    displayfeature sets color mode everytime the screen is turned on
    defaulting to Vivid mode. Having this fixed it finally allows us to use Miui color modes in AOSP.
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I7faa409bf7069108f5bed64488b2a7c5566cd921
    
    duchamp: Link displayfeature binder to death
    
    If displayfeature HAL crashes and restarts, we must recreate our
    IDisplayFeature instance.
    
    Change-Id: Icefc248bda251126f66204e4f1ced2ae4a1601c6
    
    duchamp: migrate to dislayfeature AIDL
    
    duchamp: Introduce displayfeature color service and use MIUI color modes
    
    This service relays the current AOSP color mode to the Xiaomi
    displayfeature HAL, matching stock behaviour. This is necessary to
    avoid colors being reset after turning the screen off, because
    displayfeature sets color mode everytime the screen is turned on
    defaulting to Vivid mode. Having this fixed it finally allows us to use Miui color modes in AOSP.
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I7faa409bf7069108f5bed64488b2a7c5566cd921
    
    duchamp: Link displayfeature binder to death
    
    If displayfeature HAL crashes and restarts, we must recreate our
    IDisplayFeature instance.
    
    Change-Id: Icefc248bda251126f66204e4f1ced2ae4a1601c6
    
    duchamp: migrate to dislayfeature AIDL
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Co-Authored-By: lolipuru <banstellazz@gmail.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,10 +22,13 @@
 package org.lineageos.settings;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.hardware.display.DisplayManager;
 import android.util.Log;
 import android.view.Display;
@@ -33,6 +36,7 @@ import android.view.Display.HdrCapabilities;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.display.ColorModeService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingService;
@@ -79,6 +83,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     private void startServices(Context context) {
         if (DEBUG) Log.i(TAG, "Starting services...");
+
+        // Start Color Mode Service
+        context.startServiceAsUser(new Intent(context, ColorModeService.class), UserHandle.CURRENT);
 
         // Start Touch Sampling Service
         context.startServiceAsUser(new Intent(context, TouchSamplingService.class), UserHandle.CURRENT);
