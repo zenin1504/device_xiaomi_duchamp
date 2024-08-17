245bb344a306effd0fff8976c4f3ad4f10b9add2
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 03:05:49 +0300

    duchamp: Use prebuilt libcodec2_soft_common.so for Dolby
    F DEBUG   : *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
    F DEBUG   : LineageOS Version: 'unknown'
    F DEBUG   : Build fingerprint: 'POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.101.0.VNLMIXM:user/release-keys'
    F DEBUG   : Revision: '0'
    F DEBUG   : ABI: 'arm64'
    F DEBUG   : Timestamp: 2025-03-14 23:30:23.431447077+0530
    F DEBUG   : Process uptime: 1s
    F DEBUG   : Cmdline: /vendor/bin/hw/vendor.dolby.media.c2@1.0-service
    F DEBUG   : pid: 8941, tid: 13689, name: HwBinder:8941_7  >>> /vendor/bin/hw/vendor.dolby.media.c2@1.0-service <<<
    
    Change-Id: I7a94241b1b2874713f948879df45b26f30576491
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- extract-files.py
+++ extract-files.py
@@ -98,6 +98,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so')
         .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
 
+    ('vendor/lib64/libcodec2_soft_ac4dec.so', 'vendor/lib64/libcodec2_soft_ddpdec.so'): blob_fixup()
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common_prebuilt.so'),
+
      ('odm/lib64/libmt_mitee.so',
       'vendor/bin/hw/android.hardware.security.keymint@3.0-service.mitee'): blob_fixup()
          .replace_needed('android.hardware.security.keymint-V3-ndk.so', 'android.hardware.security.keymint-V4-ndk.so'),
