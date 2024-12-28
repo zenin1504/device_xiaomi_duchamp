1dff6cf6701df7a8bb4c196bca7049380c830674
Arian <arian.kulmer@web.de>
2024-12-28 20:49:24 +0100

    duchamp: udfps: Compare against the aidl AcquiredInfo
    Change-Id: I5b6eb5a4544373d71b89abc0e301126eb55e31d1

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -6,6 +6,7 @@
 
 #define LOG_TAG "UdfpsHandler.xiaomi_mt6897"
 
+#include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
 #include <android-base/logging.h>
 #include <android-base/unique_fd.h>
 
@@ -35,6 +36,8 @@
 #define DISP_FEATURE_PATH "/dev/mi_display/disp_feature"
 
 #define FOD_PRESS_STATUS_PATH "/sys/class/touch/touch_dev/fod_press_status"
+
+using ::aidl::android::hardware::biometrics::fingerprint::AcquiredInfo;
 
 namespace {
 
@@ -185,7 +188,7 @@ class XiaomiMt6897UdfpsHander : public UdfpsHandler {
 
     void onAcquired(int32_t result, int32_t vendorCode) {
         LOG(INFO) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
-        if (result == FINGERPRINT_ACQUIRED_GOOD) {
+        if (static_cast<AcquiredInfo>(result) == AcquiredInfo::GOOD) {
             setFingerDown(false);
         }
     }
