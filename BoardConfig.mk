22067cd580516585183ebf76376d87d24d32cd28
rio004 <riyon336@gmail.com>
2025-01-15 22:43:19 +0000

    duchamp: Update blobs to OS2.0.1.0.VNLMIXM
    * Fixup 1a4359f2cf3b76ce6dc8eb1e1d88d04265dcef1a while we at it.
    
    Change-Id: Ib139803e2e3b350d94a4454906260dd6717ebf47
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,7 +157,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-09-05
+VENDOR_SECURITY_PATCH := 2024-12-01
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
